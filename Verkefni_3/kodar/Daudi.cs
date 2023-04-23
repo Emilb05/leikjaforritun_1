96dea521a0efc0b4f33dadea9065467111f27b04
Emilb05 <89970307+Emilb05@users.noreply.github.com>
2023-04-23 21:16:25 +0000

    Add files via upload

---
+++ Verkefni_3/kodar/Daudi.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Daudi : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("byrja");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(2);
+    }
+
+}
